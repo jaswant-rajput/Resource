2eafc2eef6526d256d0fc0ff0b1a35938d23b6f9
Mokshj204 <158202022+Mokshj204@users.noreply.github.com>
2024-05-20 06:25:24 +0530

    Created action for creating resource and resource creation is complete

--- frontend/src/components/calendar.js
+++ frontend/src/components/calendar.js
@@ -1,11 +1,8 @@
 import React from 'react'
-import { useEffect, useState } from 'react'
+//import { useEffect, useState } from 'react'
 import './calendar.css'
 import Navbar from './navbar'
 const Calendar = () => {
-
-
-   // let dummy = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31];
 
    let dummy = [
     { day: 1, name: 'Day 1' },
