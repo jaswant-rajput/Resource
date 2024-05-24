03cbd28963c8b685b909b3e6d9a719cc2be27b39
Mokshj204 <158202022+Mokshj204@users.noreply.github.com>
2024-05-24 21:45:43 +0530

    Deletion UI done

--- frontend/src/actions/resourceAllocationActions.js
+++ frontend/src/actions/resourceAllocationActions.js
@@ -1,8 +1,8 @@
-import { ENDPOINT_URL } from "../constants/constant
+import { ENDPOINT_URL } from "../constants/constant"
 
 export const getAllocationByMonth = (id,year,month) => {
 console.log(year,month);
-    return fetch(`${ENDPOINT_URL}/get-allocation-by-month?year=${year}&month=${month}&id=${id}`, {
+    return fetch(`${ENDPOINT_URL}/get-allocation-by-month?year=${year}&month=${month}&id=${id}`, { //id for resource 
         method: "GET",
         headers: {
             Accept: 'application/json',
@@ -24,3 +24,30 @@ export const getAllocationId = (id,startdate,enddate) => {
         .then(response => response.json())
         .catch(err => console.log(err))
 }
+
+export const addAllocation = (resourceObjectId, dates, allocation) => {
+    return fetch(`${ENDPOINT_URL}/add-allocation`, {
+      method: "POST",
+      headers: {
+        Accept: 'application/json',
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        resourceObjectId,
+        dates,
+        allocation
+      })
+    })
+      .then(response => response.json())
+      .then(data => {
+        if (data.success) {
+          return data.status;
+        } else {
+          throw new Error(data.error);
+        }
+      })
+      .catch(err => {
+        console.error('Error adding allocation:', err);
+        throw err;
+      });
+};
--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,275 +1,3 @@
-// import * as React from 'react';
-// import TextField from '@mui/material/TextField';
-// import Autocomplete from '@mui/material/Autocomplete';
-// import Dialog from '@mui/material/Dialog';
-// import DialogActions from '@mui/material/DialogActions';
-// import DialogContent from '@mui/material/DialogContent';
-// import DialogTitle from '@mui/material/DialogTitle';
-// import Button from '@mui/material/Button';
-// import MenuItem from '@mui/material/MenuItem';
-// import Select from '@mui/material/Select';
-// import Alert from '@mui/material/Alert';
-// import { getAllResources } from '../actions/resourceAllocationActions';
-// import { createResource } from '../actions/resourceActions';
-// import { useEffect, useState } from 'react';
-
-// export default function ComboBox() {
-  // const [resources, setResources] = useState([]);
-  // const [open, setOpen] = useState(false);
-  // const [resourceOpen, setResourceOpen] = useState(false);
-  // const [allocationOpen, setAllocationOpen] = useState(false);
-  // const [singleDayOpen, setSingleDayOpen] = useState(false);
-  // const [multipleDayOpen, setMultipleDayOpen] = useState(false);
-  // const [defaultAllocationOpen, setDefaultAllocationOpen] = useState(false);
-
-  // const [resourceType, setResourceType] = useState('');
-  // const [resourceNo, setResourceNo] = useState('');
-
-  // const [alertMessage, setAlertMessage] = useState('');
-  // const [alertOpen, setAlertOpen] = useState(false);
-
-
-
-//   useEffect(() => {
-//     fetchData();
-//   }, []);
-
-
-//   const fetchData = async () => {
-//     try {
-//       const response = await getAllResources();
-//       console.log('data from getAllResources', response);
-
-//       if (response && response.data) {
-//         const transformedData = response.data.map(resource => ({
-//           label: `${resource.resourceType} - ${resource.resourceNo}`,
-//           resourceNo: resource.resourceNo,
-//           resourceType: resource.resourceType
-//         }));
-//         setResources(transformedData);
-//       } else {
-//         console.error('Invalid data format:', response);
-//       }
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   };
-
-
-  // const handleOpen = () => {
-  //   setOpen(true);
-  // };
-
-  // const handleClose = () => {
-  //   setOpen(false);
-  // };
-
-  // const handleResourceOpen = () => {
-  //   setResourceOpen(true);
-  //   setOpen(false); // Close the first modal
-  // };
-
-  // const handleResourceClose = () => {
-  //   setResourceOpen(false);
-  // };
-
-  // const handleAllocationOpen = () => {
-  //   setAllocationOpen(true);
-  //   setOpen(false); // Close the first modal
-  // };
-
-  // const handleAllocationClose = () => {
-  //   setAllocationOpen(false);
-  // };
-
-  // const handleSingleDayOpen = () => {
-  //   setSingleDayOpen(true);
-  //   setAllocationOpen(false);
-  // };
-
-  // const handleSingleDayClose = () => {
-  //   setSingleDayOpen(false);
-  // };
-
-  // const handleMultipleDayOpen = () => {
-  //   setMultipleDayOpen(true);
-  //   setAllocationOpen(false);
-  // };
-
-  // const handleMultipleDayClose = () => {
-  //   setMultipleDayOpen(false);
-  // };
-
-  // const handleDefaultAllocationOpen = () => {
-  //   setDefaultAllocationOpen(true);
-  //   setAllocationOpen(false);
-  // };
-
-  // const handleDefaultAllocationClose = () => {
-  //   setDefaultAllocationOpen(false);
-  // };
-
-  // const handleResourceTypeChange = (event) => {
-  //   setResourceType(event.target.value);
-  // };
-
-//   const handleAlertClose = () => {
-//     setAlertOpen(false);
-//   };
-
-//   const triggerAlert = (message) => {
-//     setAlertMessage(message);
-//     setAlertOpen(true);
-//   };
-
-
-//   const handleResourceCreateSave = () => {
-//     // Check if resource combination exists
-//     const resourceExists = resources.some(
-//       resource => resource.resourceNo === resourceNo && resource.resourceType === resourceType
-//     );
-
-//     if (resourceExists) {
-//       triggerAlert('This resource combination already exists.');
-//     } else {
-//       createResource({ resourceNo, resourceType })
-//         .then(data => {
-//           console.log('Successful resource creation', data);
-//           // Optionally, refetch resources to update the list
-//           fetchData();
-//           handleResourceClose();
-//         })
-//         .catch(err => {
-//           console.log('Error in creating resource', err);
-//           triggerAlert('Failed to create resource.');
-//         });
-//     }
-//   };
-
-// return (
-//   <div>
-//     <div>
-//       <button type="button" className="btn btn-primary rounded-pill" style={{ width: '16vw', marginBottom: '1.2vw', marginTop: '0.8vw' }} onClick={handleOpen}>
-//         Create
-//       </button>
-//     </div>
-
-//     <Autocomplete disablePortal id="combo-box-demo" options={resources} sx={{ marginRight: '0.5vw', marginLeft: '0.5vw' }}
-//       renderInput={(params) => <TextField {...params} label="Resources" />}
-//     />
-
-//     <Dialog open={open} onClose={handleClose}>
-//       <DialogTitle>Select an Option</DialogTitle>
-//       <DialogContent>
-//         <ul>
-//           <li>
-//             <Button onClick={handleResourceOpen}>Resource</Button>
-//           </li>
-//           <br></br>
-//           <li>
-//             <Button onClick={handleAllocationOpen}>Allocation</Button>
-//           </li>
-//         </ul>
-//       </DialogContent>
-//       <DialogActions>
-//         <Button onClick={handleClose}>Close</Button>
-//       </DialogActions>
-//     </Dialog>
-
-//     <Dialog open={resourceOpen} onClose={handleResourceClose}>
-//       <DialogTitle>Resource Modal</DialogTitle>
-//       <DialogContent>
-//         <Select value={resourceType} onChange={handleResourceTypeChange} displayEmpty fullWidth>
-//           <MenuItem value="" disabled>
-//             Resource Type
-//           </MenuItem>
-//           <MenuItem value="classroom">Classroom</MenuItem>
-//           <MenuItem value="lab">Lab</MenuItem>
-//           <MenuItem value="seminar hall">Seminar Hall</MenuItem>
-//         </Select>
-//         <br />
-//         <TextField
-//           margin="dense"
-//           label="Resource Number"
-//           type="text"
-//           fullWidth
-//           variant="outlined"
-//           onChange={(e) => setResourceNo(e.target.value)}
-//         />
-//       </DialogContent>
-//       <DialogActions>
-//         <Button onClick={handleResourceCreateSave}>Save</Button>
-//         <Button onClick={handleResourceClose}>Close</Button>
-//       </DialogActions>
-//     </Dialog>
-
-//     <Dialog open={allocationOpen} onClose={handleAllocationClose}>
-//       <DialogTitle>Allocation Modal</DialogTitle>
-//       <DialogContent>
-//         <ul>
-//           <li>
-//             <Button onClick={handleSingleDayOpen}>Single Day Allocation</Button>
-//           </li>
-//           <br></br>
-//           <li>
-//             <Button onClick={handleMultipleDayOpen}>Multiple Day Allocation</Button>
-//           </li>
-//           <br></br>
-//           <li>
-//             <Button onClick={handleDefaultAllocationOpen}>Set Default Allocation</Button>
-//           </li>
-//         </ul>
-//       </DialogContent>
-//       <DialogActions>
-//         <Button onClick={handleAllocationClose}>Close</Button>
-//       </DialogActions>
-//     </Dialog>
-
-//     <Dialog open={singleDayOpen} onClose={handleSingleDayClose}>
-//       <DialogTitle>Single Day Allocation</DialogTitle>
-//       <DialogContent>
-//         {/* Add your single day allocation components here */}
-//       </DialogContent>
-//       <DialogActions>
-//         <Button onClick={handleSingleDayClose}>Close</Button>
-//       </DialogActions>
-//     </Dialog>
-
-//     <Dialog open={multipleDayOpen} onClose={handleMultipleDayClose}>
-//       <DialogTitle>Multiple Day Allocation</DialogTitle>
-//       <DialogContent>
-//         {/* Add your multiple day allocation components here */}
-//       </DialogContent>
-//       <DialogActions>
-//         <Button onClick={handleMultipleDayClose}>Close</Button>
-//       </DialogActions>
-//     </Dialog>
-
-//     <Dialog open={defaultAllocationOpen} onClose={handleDefaultAllocationClose}>
-//       <DialogTitle>Set Default Allocation</DialogTitle>
-//       <DialogContent>
-//         {/* Add your default allocation components here */}
-//       </DialogContent>
-//       <DialogActions>
-//         <Button onClick={handleDefaultAllocationClose}>Close</Button>
-//       </DialogActions>
-//     </Dialog>
-
-
-
-//     {/*alert */}
-//     <Dialog open={alertOpen} onClose={handleAlertClose}>
-//       <Alert severity="warning" onClose={handleAlertClose}>
-//         {alertMessage}
-//       </Alert>
-//     </Dialog>
-//   </div>
-// );
-// }
-
-
-
-
 import * as React from 'react';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
@@ -284,8 +12,7 @@ import MenuItem from '@mui/material/MenuItem';
 import { getAllResources ,createResource } from '../actions/resourceActions';
 import { useEffect, useState } from 'react';
 
-export default function ComboBox() {
-  const [resources, setResources] = useState([]);
+const Navbar=({ onResourceSelect }) => {
   const [open, setOpen] = useState(false);
   const [resourceOpen, setResourceOpen] = useState(false);
   const [allocationOpen, setAllocationOpen] = useState(false);
@@ -293,8 +20,11 @@ export default function ComboBox() {
   const [multipleDayOpen, setMultipleDayOpen] = useState(false);
   const [defaultAllocationOpen, setDefaultAllocationOpen] = useState(false);
 
+  const [resources, setResources] = useState([]);
   const [resourceType, setResourceType] = useState('');
   const [resourceNo, setResourceNo] = useState('');
+
+
 
   const [alertMessage, setAlertMessage] = useState('');
   const [severity, setSeverity] = useState('');
@@ -309,7 +39,8 @@ export default function ComboBox() {
         const transformedData = response.data.map(resource => ({
           label: `${resource.resourceType} - ${resource.resourceNo}`,
           resourceNo: resource.resourceNo,
-          resourceType: resource.resourceType
+          resourceType: resource.resourceType,
+          _id : resource._id
         }));
         setResources(transformedData); // Make sure this updates the state correctly
       } else {
@@ -392,6 +123,15 @@ export default function ComboBox() {
     setAlertOpen(true);
   };
 
+  const logSelectedResourceId = (resource) => {
+    if (resource) {
+      console.log('Selected Resource ID:', resource._id);
+     onResourceSelect(resource._id);
+    } else {
+      console.log('No resource selected');
+    }
+  };
+
   const handleResourceCreateSave = async () => {
     console.log('Checking if resource exists:', { resourceNo, resourceType });
 
@@ -432,8 +172,14 @@ export default function ComboBox() {
         </button>
       </div>
 
-      <Autocomplete disablePortal id="combo-box-demo" options={resources} sx={{ marginRight: '0.5vw', marginLeft: '0.5vw' }}
+      <Autocomplete
+        disablePortal
+        id="combo-box-demo"
+        options={resources}
+        getOptionLabel={(option) => `${option.resourceType} - ${option.resourceNo}`}
+        sx={{ marginRight: '0.5vw', marginLeft: '0.5vw' }}
         renderInput={(params) => <TextField {...params} label="Resources" />}
+        onChange={(event, value) => logSelectedResourceId(value)}
       />
 
       <Dialog open={open} onClose={handleClose}>
@@ -545,3 +291,6 @@ export default function ComboBox() {
   );
 
 }
+
+
+export default Navbar
