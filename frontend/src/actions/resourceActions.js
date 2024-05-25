b2411897c43569ae7ddb0609bc9e0d554ad6b517
pagasis <74396469+Pagasis@users.noreply.github.com>
2024-05-25 21:46:55 +0530

    added allocation actions

--- frontend/src/actions/resourceActions.js
+++ frontend/src/actions/resourceActions.js
@@ -1,38 +1,36 @@
 import { ENDPOINT_URL } from "../constants/constant";
 
 export const createResource = (resourceData) => {
-  return fetch(`${ENDPOINT_URL}/create-resource`, {
-    method: "POST",
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(resourceData)
-  })
+  	return fetch(`${ENDPOINT_URL}/create-resource`, {
+		method: "POST",
+		headers: {
+			Accept: 'application/json',
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(resourceData)
+  	})
     .then(response => response.json())
     .then(data => {
-      if (data.success) {
-        return data;
-      } else {
-        throw new Error(data.error);
-      }
+      	if (data.success) {
+        	return data;
+      	} else {
+        	throw new Error(data.error);
+      	}
     })
     .catch(err => {
-      console.error("Error creating resource:", err);
-      throw err; // Rethrow error for caller to handle
-    });
-};
-
-
-export const getAllResources = () => {
-  return fetch(`${ENDPOINT_URL}/get-all-resources`, {
-      method: "GET",
-      headers: {
-          Accept: 'application/json',
-          'Content-type': 'application/json'
-      }
-  })
-      .then(response => response.json())
-      .catch(err => console.log(err))
+      	console.error("Error creating resource:", err);
+      	throw err; // Rethrow error for caller to handle
+    })
 }
 
+export const getAllResources = () => {
+	return fetch(`${ENDPOINT_URL}/get-all-resources`, {
+		method: "GET",
+		headers: {
+			Accept: 'application/json',
+			'Content-type': 'application/json'
+		}
+	})
+		.then(response => response.json())
+		.catch(err => console.log(err))
+}
