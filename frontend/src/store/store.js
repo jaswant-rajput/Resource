f353fb966ecd648b5d57be651d8675f886adf2d3
Mokshj204 <158202022+Mokshj204@users.noreply.github.com>
2024-06-25 00:03:54 +0530

    Trial Commit

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,10 +1,23 @@
-import { create } from 'zustand';
+// import { create } from 'zustand';
 
-export const useAuthStore = create((set) => ({
-    user: null,
-    setUser : (data) => set((state) => ({ user : data })),
-    setUserFromLocalStorage : (data) => set((state) => ({ user : data })),
-    // updateUserProfileStatus : (data) => set((state) => ({ user: data})),
-}))
-// access data : `const user = useAuthStore.getState().user`
-// access set method : `const setStateUser = useAuthStore((state) => state.setUser);`
+// export const useAuthStore = create((set) => ({
+//     user: null,
+//     setUser : (data) => set((state) => ({ user : data })),
+//     setUserFromLocalStorage : (data) => set((state) => ({ user : data })),
+//     // updateUserProfileStatus : (data) => set((state) => ({ user: data})),
+// }))
+// // access data : `const user = useAuthStore.getState().user`
+// // access set method : `const setStateUser = useAuthStore((state) => state.setUser);`
+
+
+import create from 'zustand';
+
+const useAuthStore = create((set) => ({
+  user: JSON.parse(localStorage.getItem('user')) || null,
+  setUser: (user) => set(() => {
+    localStorage.setItem('user', JSON.stringify(user));
+    return { user };
+  }),
+}));
+
+export { useAuthStore };
